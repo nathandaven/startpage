7142fd6df7ceb6db012bd2dd2bad981c49150b72
nathandaven <nathandaven@gmail.com>
2024-10-31 23:56:52 -0400

    add geolocation & localstorage setting for name and location

--- README.md
+++ README.md
@@ -8,26 +8,46 @@ This repository includes my API key. The API key is attached to a dummy account,
 
 Firefox extension is compiled and signed using web-ext. Please read the documentation on web-ext if you are interested in creating and signing your own version of this project.
 
+## Local Storage options
+
+You can now set your name, lat, and longitude by local storage. The weather will default to the user's location if given, but will use these as a fall back.
+
+```js
+localStorage.setItem("lat", "33.7490");  // atlanta
+localStorage.setItem("lon", "-84.3880"); // atlanta
+localStorage.setItem("name", "nathan");  // me
+```
+
+This will persist across sessions.
+
+## Web-ext credentials
+
+Get credentials [here](https://addons.mozilla.org/developers/addon/api/key/) to build the web web extension.
+
+See [here](https://extensionworkshop.com/documentation/develop/getting-started-with-web-ext/) for documentation from Mozilla.
+
+To build without signing: `npm install` -> `npm run build`
+
+To build with signing: `npm install` -> `npm run sign`
 
 ## Credits
 
 Complete credit goes to these developers and their sources:
 
-- Searching mechanism: 
-https://github.com/nathandaven/tilde
+- Searching mechanism:
+  https://github.com/nathandaven/tilde
 
 - General design:
-https://github.com/nathandaven/startpage
+  https://github.com/nathandaven/startpage
 
-- Focus content on newtab script: 
-https://github.com/gdh1995/vimium-c-helpers/blob/master/newtab/newtab.js
+- Focus content on newtab script:
+  https://github.com/gdh1995/vimium-c-helpers/blob/master/newtab/newtab.js
 
 - My custom Firefox userChrome.css based on this [project](https://github.com/ranmaru22/firefox-vertical-tabs):  
-https://gist.github.com/nathandaven/67f293758f13971882adf334b66db8ed
+  https://gist.github.com/nathandaven/67f293758f13971882adf334b66db8ed
 
-- My Tab Centern Reborn custom css config: 
-https://gist.github.com/nathandaven/93f52bdc881a6e7cc621e097404b0d01
-
+- My Tab Centern Reborn custom css config:
+  https://gist.github.com/nathandaven/93f52bdc881a6e7cc621e097404b0d01
 
 ## Screenshots
 
--- js/weather.js
+++ js/weather.js
@@ -1,89 +1,118 @@
+let lat = localStorage.getItem("lat") ?? 33.749;
+let lon = localStorage.getItem("lon") ?? -84.388;
 
-    // ----- CHANGE THESE VARIABLES
+// ----- DON'T TOUCH ANYTHING BELOW THIS UNLESS YOU KNOW WHAT YOU'RE DOING
+// ----- CHANGE THESE VARIABLES
 
-    // Do you want to show the weather?
-    const weather = true;
-    // Do you want animations?
-    const animated = true;
+// Read the documentation for setting the weather API key
+const apiKey = "904449963dd1c92c21836b8f93e53829";
+const units = "imperial";
 
+// Do you want to show the weather?
+const weather = true;
+// Do you want animations?
+const animated = true;
+navigator.geolocation.getCurrentPosition(
+  (position) => {
+    lat = position.coords.latitude;
+    lon = position.coords.longitude;
+    positionRetrieved = true;
+    setWeather();
 
+    console.log(`Latitude: ${lat}, Longitude: ${lon}`);
+  },
+  (error) => {
+    console.error("Error getting geolocation:", error);
+    setWeather();
+  }
+);
 
-    // Read the documentation for setting the weather API key
-    const apiKey = "904449963dd1c92c21836b8f93e53829";
-    const lat = 33.7490;
-    const lon = -84.3880;
-    const units = "imperial";
+function setWeather() {
+  var weatherEl = document.getElementById("weatherDesc");
+  var tempEl = document.getElementById("temp");
 
-    // ----- DON'T TOUCH ANYTHING BELOW THIS UNLESS YOU KNOW WHAT YOU'RE DOING
+  if (weather) {
+    var apiLink =
+      "https://api.openweathermap.org/data/2.5/weather?lat=" +
+      lat +
+      "&lon=" +
+      lon +
+      "&units=" +
+      units +
+      "&appid=" +
+      apiKey;
+    console.log(apiLink);
+    // grabs json data from url, thanks https://stackoverflow.com/a/35970894
+    var getJSON = function (url, callback) {
+      var xhr = new XMLHttpRequest();
+      xhr.open("GET", url, true);
+      xhr.responseType = "json";
+      xhr.onload = function () {
+        var status = xhr.status;
+        if (status === 200) {
+          callback(null, xhr.response);
+        } else {
+          callback(status, xhr.response);
+        }
+      };
+      xhr.send();
+    };
 
-    var weatherEl = document.getElementById("weatherDesc");
-    var tempEl = document.getElementById("temp");
-
-    if (weather) {
-        var apiLink =
-            "https://api.openweathermap.org/data/2.5/weather?lat=" +
-            lat +
-            "&lon=" +
-            lon +
-            "&units=" +
-            units +
-            "&appid=" +
-            apiKey;
-        console.log(apiLink);
-        // grabs json data from url, thanks https://stackoverflow.com/a/35970894
-        var getJSON = function (url, callback) {
-            var xhr = new XMLHttpRequest();
-            xhr.open("GET", url, true);
-            xhr.responseType = "json";
-            xhr.onload = function () {
-                var status = xhr.status;
-                if (status === 200) {
-                    callback(null, xhr.response);
-                } else {
-                    callback(status, xhr.response);
-                }
-            };
-            xhr.send();
-        };
-
-        // gets city name from weather api
-        getJSON(apiLink, function (err, data) {
-            if (err !== null) {
-                weatherEl.innerText = "";
-                tempEl.innerText = "";
-            } else {
-                var city = data.name;
-                var temp = data.main.temp;
-                var hiTemp = data.main.temp_max;
-                var loTemp = data.main.temp_min;
-                var weatherDesc = data.weather[0].description;
-                weatherEl.innerHTML =
-                    weatherDesc + " in " + city;
-                tempEl.innerHTML = "h: " + hiTemp + "° / c: " + temp + "° / l: " + loTemp + "°";
-            }
-        });
-    } else {
+    // gets city name from weather api
+    getJSON(apiLink, function (err, data) {
+      if (err !== null) {
         weatherEl.innerText = "";
         tempEl.innerText = "";
+      } else {
+        var city = data.name;
+        var temp = data.main.temp;
+        var hiTemp = data.main.temp_max;
+        var loTemp = data.main.temp_min;
+        var weatherDesc = data.weather[0].description;
+        weatherEl.innerHTML = weatherDesc + " in " + city;
+        tempEl.innerHTML =
+          "h: " + hiTemp + "° / c: " + temp + "° / l: " + loTemp + "°";
+      }
+    });
+  } else {
+    weatherEl.innerText = "";
+    tempEl.innerText = "";
+  }
+}
+
+// Setting a cookie
+function setCookie(name, value, daysToExpire) {
+  document.cookie = name + "=" + value + "; ";
+}
+
+// Retrieving a cookie
+function getCookie(name) {
+  const cookies = document.cookie.split(";");
+  for (let i = 0; i < cookies.length; i++) {
+    const cookie = cookies[i].trim();
+    if (cookie.startsWith(name + "=")) {
+      return cookie.substring(name.length + 1);
     }
+  }
+  return null;
+}
 
+if (animated) {
+  // hides all elements
+  document.querySelectorAll("*").forEach((el) => (el.style.opacity = 0));
 
-    if (animated) {
-        // hides all elements
-        document.querySelectorAll("*").forEach(el => el.style.opacity = 0);
+  // anime.js animation function
+  function playAnimation() {
+    var tl = anime.timeline({
+      easing: "easeInOutExpo",
+      duration: 400 /*1200*/,
+    });
 
-        // anime.js animation function
-        function playAnimation() {
-            var tl = anime.timeline({
-                easing: "easeInOutExpo",
-                duration: 400, /*1200*/
-            });
-
-            tl.add({
-                targets: "#img",
-                opacity: [0, 1],
-                translateY: [100, 0],
-            }) /*
+    tl.add({
+      targets: "#img",
+      opacity: [0, 1],
+      translateY: [100, 0],
+    }) /*
                 .add(
                     {
                         targets: "#img",
@@ -91,22 +120,24 @@
                     },
                     "-=400"
                 )*/
-                .add(
-                    {
-                        targets: "main *",
-                        opacity: [0, 1],
-                        translateY: [10, 0],
-                        delay: anime.stagger(5),
-                    },
-                    /*"-=1200"*/
-                    "-=400"
-                );
-        }
+      .add(
+        {
+          targets: "main *",
+          opacity: [0, 1],
+          translateY: [10, 0],
+          delay: anime.stagger(5),
+        },
+        /*"-=1200"*/
+        "-=400"
+      );
+  }
 
-        window.onload = function () {
-            document.querySelectorAll("*").forEach(el => el.style.opacity = 1);
-            playAnimation();
-        }
-
-
-    }
+  window.onload = function () {
+    // set cookie with localStorage.setItem("name", "john doe") in browser console
+    const name = localStorage.getItem("name");
+    var weatherEl = document.getElementById("name");
+    weatherEl.innerText = name ?? "browser";
+    document.querySelectorAll("*").forEach((el) => (el.style.opacity = 1));
+    playAnimation();
+  };
+}
